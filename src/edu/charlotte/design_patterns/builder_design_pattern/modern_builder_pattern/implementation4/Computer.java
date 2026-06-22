f13a4d0c4ede8714f000b1f41b8b5bc9747d6085
Phanindra08 <kothuriphanindra1999@gmail.com>
2026-06-21 20:27:03 -0400

    Created an example for modern Builder Design Pattern implementation 4

---
+++ src/edu/charlotte/design_patterns/builder_design_pattern/modern_builder_pattern/implementation4/Computer.java
@@ -0,0 +1,50 @@
+package edu.charlotte.design_patterns.builder_design_pattern.modern_builder_pattern.implementation4;
+
+// Product
+class Computer {
+    private final String cpu;
+    private final String ram;
+    private final String storage;
+
+    public Computer(String cpu, String ram, String storage) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+    }
+
+    public void displayInfo() {
+        System.out.println("Computer Configuration:\n"
+                + "\tCPU: " + cpu + "\n"
+                + "\tRAM: " + ram + "\n"
+                + "\tStorage: " + storage + "\n");
+    }
+
+    public static class GamingComputerBuilder implements Builder {
+        private String cpu;
+        private String ram;
+        private String storage;
+
+        @Override
+        public Builder setCPU(String cpu) {
+            this.cpu = cpu;
+            return this;
+        }
+
+        @Override
+        public Builder setRAM(String ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        @Override
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        @Override
+        public Computer build() {
+            return new Computer(cpu, ram, storage);
+        }
+    }
+}
